68c32aa87f8f47b267ec62c226ef3ad05b414348
bimri <bimri@outlook.com>
2022-01-07 01:40:07 +0300

    Creating a Flask Application with SQLAlchemy

---
+++ chapter_2/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/<db_name>.db"
+db = SQLAlchemy(app)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
