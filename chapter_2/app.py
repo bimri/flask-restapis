d895cf422932fcc87f546f676f735fed166d846d
bimri <bimri@outlook.com>
2022-01-07 01:48:51 +0300

    defining `author` model - 3 fields [ID, name, specialization]

--- chapter_2/app.py
+++ chapter_2/app.py
@@ -6,5 +6,20 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/<db_name>.db"
 db = SQLAlchemy(app)
 
 
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    specialization = db.Column(db.String(50))
+
+    def __init__(self, name, specialization):
+        self.name = name
+        self.specialization = specialization
+
+    def __repr__(self):
+        return "<Product %d>" % self.id
+
+
+db.create_all()
+
 if __name__ == "__main__":
     app.run(debug=True)
